6331f16d9508e65facd2d0aabd82355c66637d6a
Arjun Raj <71980597+zuko046@users.noreply.github.com>
2023-12-27 12:08:18 +0530

    bbb

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -41,7 +41,7 @@ const Settings: React.FC = () => {
     <div className="rounded-sm border border-stroke bg-white px-5 pt-6 pb-2.5 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:pb-1">
       <div className="flex justify-end mb-7">
         <Link
-          to="/addrange"
+          to="/admin/addrange"
           className="inline-flex items-center justify-center rounded-full bg-primary py-4 px-10 text-center font-medium text-white hover:bg-opacity-90 lg:px-5 xl:px-5"
         >
           Add Range
